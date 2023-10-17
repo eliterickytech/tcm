d81bd1be33d3edf3f5def6265b1c165e0857e2c5
marcosaraujo-dev <marcos.araso@hotmail.com>
2023-10-16 22:17:42 -0300

    Criação na page community

---
+++ src/1-Presentation/1.1-Site/TCM.Presentation.Site/Models/FriendsActivities.cs
@@ -0,0 +1,12 @@
+﻿namespace TCM.Presentation.Site.Models
+{
+    public class FriendsActivities
+    {
+        public int Id { get; set; }
+        public int FriendId { get; set; }
+        public string FriendName { get; set; }
+        public string FriendActionDescription { get; set;}
+
+        public string FriendActionDate { get; set; }
+    }
+}
