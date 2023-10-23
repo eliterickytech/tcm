6613c18de7c2e45a844fbc142ff416914777220a
Ricardo Macedo Perdigão <57601338+eliterickytech@users.noreply.github.com>
2023-10-23 19:07:17 -0300

    Tipo de campo do chat está alterado.

--- src/3-Infrastructure/3.2-TCM-Base/TCM.Infrastructure.Data.Base/Tables/Chat.sql
+++ src/3-Infrastructure/3.2-TCM-Base/TCM.Infrastructure.Data.Base/Tables/Chat.sql
@@ -3,7 +3,7 @@
 	[Id] INT NOT NULL PRIMARY KEY	IDENTITY(1,1)
 ,	[UserId]				INT NOT NULL 
 ,	[ConnectionUserId]		INT NOT NULL
-,   [Message]               VARCHAR(1000) NOT NULL
+,   [Message]               NVARCHAR(1000) NOT NULL
 ,	[IsReaded]				BIT NOT NULL DEFAULT 0, 
     [Enabled] BIT NOT NULL DEFAULT 1, 
     [CreatedDate] DATETIME NOT NULL DEFAULT getdate(), 
