d191bffa80c4f1f74f2775428a078c0a43088032
Ricardo Macedo Perdigão <rickyteck@hotmail.com>
2023-11-26 22:51:31 -0300

    Adicionado logoff

---
+++ src/1-Presentation/1.1-Site/TCM.Presentation.Site/Controllers/Logout/LogoffController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TCM.Presentation.Site.Controllers.Logout
+{
+    public class LogoffController : Controller
+    {
+        public IActionResult Index()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Login");
+        }
+    }
+}
