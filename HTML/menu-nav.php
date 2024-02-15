b1ba9972289ffcc148227e03b6aca6a4ea071c16
Ricardo Macedo Perdigão <57601338+eliterickytech@users.noreply.github.com>
2024-02-14 22:22:47 -0300

    Delete HTML directory

--- HTML/menu-nav.php
+++
@@ -1,8 +0,0 @@
-<ul>
-    <a target="_self" href="<?php echo "https://thechefmelo.com/my_profile.php?token=".$access_token_seting . "&v=".$time;?>">
-        <li>Permissions</li>
-    </a>
-    <a href="#">
-        <li>Support</li>
-    </a>
-</ul>
